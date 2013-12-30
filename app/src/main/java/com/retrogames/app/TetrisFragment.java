cea18b8e345d192bacfa3b00178492cb6bb05a93
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2013-12-30 13:14:15 +0100

    Initial commit

---
+++ app/src/main/java/com/retrogames/app/TetrisFragment.java
@@ -0,0 +1,23 @@
+package com.retrogames.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tomasz on 30.12.13.
+ */
+public class TetrisFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.choose_game_tetris, container, false);
+    }
+}
